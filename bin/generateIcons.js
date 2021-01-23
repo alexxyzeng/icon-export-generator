2ae5feeea3c60bbbaad44aee8177e8d17f22fdb7
alex <xiayao.zeng@gmail.com>
2021-01-23 14:19:01 +0800

    feat: add support for extra icons

--- bin/generateIcons.js
+++ bin/generateIcons.js
@@ -1,49 +1,65 @@
 #!/usr/bin/env node
-const fs = require('fs')
-const path = require('path')
-const projectIcons = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'project.json'), 'utf-8'))
-const antdIcons = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'antd.json'), 'utf-8'))
-const result = antdIcons.concat(projectIcons)
+const fs = require('fs');
+const path = require('path');
+const projectIcons = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, 'project.json'), 'utf-8')
+);
+const antdIcons = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, 'antd.json'), 'utf-8')
+);
+let result = antdIcons.concat(projectIcons);
 
-let icons = {}
+const [extraIconPath] = process.argv.slice(2);
+if (extraIconPath && fs.existsSync(extraIconPath)) {
+  result = result.concat(
+    JSON.parse(fs.readFileSync(extraIconPath, 'utf-8') || '[]')
+  );
+}
+
+let icons = {};
 
 const ThemeEnum = {
   outline: 'Outline',
   fill: 'Fill',
-  twoTone: 'TwoTone'
-}
+  twoTone: 'TwoTone',
+};
 
 for (let i of result) {
   for (let j in i) {
-    const iconList = i[j]
-    iconList.forEach(icon => {
-      const { type, theme = 'outline' } = icon
-      const iconName = toUpperCase(type) + ThemeEnum[theme]
+    const iconList = i[j];
+    iconList.forEach((icon) => {
+      const { type, theme = 'outline' } = icon;
+      const iconName = toUpperCase(type) + ThemeEnum[theme];
       if (!icons[iconName]) {
-        icons[iconName] = `export { default as ${iconName} } from '@ant-design/icons/lib/${theme.toLowerCase()}/${iconName}'`
+        icons[
+          iconName
+        ] = `export { default as ${iconName} } from '@ant-design/icons/lib/${theme.toLowerCase()}/${iconName}'`;
       }
-    })
+    });
   }
 }
 
 // fs.writeFileSync('result.json', JSON.stringify(icons, null, 2))
 
-const iconList = []
+const iconList = [];
 for (let i in icons) {
-  iconList.push(icons[i])
+  iconList.push(icons[i]);
 }
 
 // const template = fs.readFileSync(path.resolve(__dirname, '..', 'template.js'), 'utf-8')
 
-fs.writeFileSync(path.resolve(process.cwd(), 'icon.js'), iconList.join('\n') + '\n')
-
+fs.writeFileSync(
+  path.resolve(process.cwd(), 'icon.js'),
+  iconList.join('\n') + '\n'
+);
 
 function toUpperCase(name) {
-  const nameList = name.split('-')
-  return nameList.map(nameItem => {
-    const initial = nameItem.slice(0, 1)
-    const restStr = nameItem.slice(1, nameItem.length)
-    return initial.toUpperCase() + restStr
-  }).join('')
+  const nameList = name.split('-');
+  return nameList
+    .map((nameItem) => {
+      const initial = nameItem.slice(0, 1);
+      const restStr = nameItem.slice(1, nameItem.length);
+      return initial.toUpperCase() + restStr;
+    })
+    .join('');
 }
-
